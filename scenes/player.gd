43d91770bd2513480339c88ef79282e9835f5507
ivanding3 <ivanding3@gmail.com>
2026-09-19 14:10:42 -0700

    Added Background and new tile texture

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,12 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("Up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	elif not Input.is_action_pressed("Up"):
-		velocity.y += 30
-		
-	var direction := Input.get_axis("Left", "Right")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
