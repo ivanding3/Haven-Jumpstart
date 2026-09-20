bf9f2e5c26b3cf4cf584ddd93d5132f87dd9298e
ivanding3 <ivanding3@gmail.com>
2026-09-20 00:29:58 -0700

    moveable boxes

--- player_movement.gd
+++ player_movement.gd
@@ -3,16 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const PUSH_SPEED = 300
 
-
+var touched_floor
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if is_on_floor():
+		touched_floor = true
 	# Handle jump.
-	if Input.is_action_pressed("Up") and is_on_floor():
+	if Input.is_action_pressed("Up") and touched_floor:
 		velocity.y = JUMP_VELOCITY
+		touched_floor = false
 	elif not Input.is_action_pressed("Up"):
 		velocity.y += 30
 		
@@ -21,5 +24,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().get_class() == "RigidBody2D":
+			var collider = collision.get_collider()
+			if collision.get_normal().y != -1:
+				collider.linear_velocity = collision.get_normal()*-1*PUSH_SPEED
+				
 	move_and_slide()
